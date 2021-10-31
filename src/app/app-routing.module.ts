b1da3ee6305911935c6857a026b390082e8e7e21
Jo Lares <jolares@ubuntu-desktop.lxd>
2021-10-31 16:14:52 +0000

    (chore) Configure entry-point module HomeModule as project-root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }, { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+
+const APP_ROUTES: Routes = [
+  { 
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) 
+  },
+  { 
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+  }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
