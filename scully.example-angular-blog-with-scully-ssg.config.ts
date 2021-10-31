95202cc87f1c586f40c32bb3f37b1599aefd26de
Jo Lares <jolares@ubuntu-desktop.lxd>
2021-10-31 16:07:53 +0000

    (chore) Add blog support using scully

--- scully.example-angular-blog-with-scully-ssg.config.ts
+++ scully.example-angular-blog-with-scully-ssg.config.ts
@@ -1,8 +1,16 @@
 import { ScullyConfig } from '@scullyio/scully';
+
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "example-angular-blog-with-scully-ssg",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
